1166aef20422aa87cdb9a33357c05506c9b1eb12
Richard <richard.glew@gmail.com>
2014-11-22 10:05:43 +1000

    hanoi

--- Week1.hs
+++ Week1.hs
@@ -27,4 +27,13 @@ validate x
 type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (n-1) c b a)
+
+{- What if we can rewrite our recurrence in the following fashion:
+
+Tn=Tn−1+1+Tn−1
+
+This tells us that to solve the tower for n disks (which will take Tn steps) we need to sove the tower for n−1 disks, then solve the tower for 1 disk and then solve the tower for n−1 disks again. You can probably already see where I am going with this, there is only a slight intuitive leap we need to make here, regarding where to transfer the disks each time. We need to transfer n−1 disks from the source peg to the pivot peg, then transfer 1 disk from the source peg to the destination peg and then transfer n−1 disks from the pivot to the destination and we're done. 
+-}
