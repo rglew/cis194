e20a828caa84d03b1952a2ed0b4cfb6ef6c376fb
Richard <richard.glew@gmail.com>
2014-12-18 06:57:09 +1000

    Week 4 changes

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,10 @@ module Main where
 import Week1.Tests
 import Week2.Tests
 import Week3.Tests
+import Week4.Tests
 
 main = do week1Tests 
           week2Tests
           week3Tests
+          week4Tests
+          
